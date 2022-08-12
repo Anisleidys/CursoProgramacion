c7cb6aeddd6a181b8db2e1b4e9b6bd96df720144
Any <anypereira88@gmail.com>
2022-08-11 23:10:39 -0300

    Pruebas de programacion V1.1 Se agregó texto Trabajo con Git

--- src/Ejemplo01_01.java
+++ src/Ejemplo01_01.java
@@ -2,6 +2,7 @@ public class Ejemplo01_01 {
     public static void main (String [] args) {
         System.out.println("Bienvenido al curso de Programación Java");
         System.out.println("Estoy aprendiendo a programar");
+        System.out.println("Trabajo con Git");
 
 
     }
