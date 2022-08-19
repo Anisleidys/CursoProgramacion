efcb94e559b97c6fdc358d3e1dbd7079d2ce6b14
Anisleidys <anypereira88@gmail.com>
2022-08-19 00:10:22 -0300

    Operadores aritmeticos

--- src/Ejemplo02_03.java
+++ src/Ejemplo02_03.java
@@ -12,11 +12,41 @@ public class Ejemplo02_03 {
         int numExpedientes=suma(numContratos,numReclamaciones);
 
         System.out.println("Tenemos " + numPersonas + " personas " + numMuebles + " muebles y " + numExpedientes + " expedientes");
+
+       int respuesta = suma(3,4);
+
+        System.out.println( "La respuesta es " + respuesta);
+
+        int numClientesJuan = 2;
+        int numClienteMaria = 3;
+        //int Total= suma(numClientesJuan, numClienteMaria);
+        System.out.println("Juan y Maria son " + suma(numClientesJuan, numClienteMaria));
+
+        suma(3,suma(numClientesJuan, numClienteMaria));
+        System.out.println("Resultado de la suma combinada es " + suma(3,suma(numClientesJuan, numClienteMaria)));
+
+        int b= suma(numClientesJuan, numClienteMaria);
+        int a= suma(3,b);
+        int x = sumaMas5(5, 10 );
+
+        int resultado = suma( a, suma(b,x));
+        System.out.println("Resultado " +resultado);
+
+
+
+
+
+
     }
     private static int suma(int a, int b){
         return a+b;
 
+    }
 
+    private static int sumaMas5(int a, int b){
+        int c=5;
+        return a + b + c;
 
     }
+
 }
---
+++ src/Ejemplo02_04.java
@@ -0,0 +1,12 @@
+public class Ejemplo02_04 {
+    public static void main (String[] args) {
+        int divisionEntera = 7/2;
+        int modulo = 7%2;
+        double divisionReal = 7.0/2.0;
+
+        System.out.println("division Entera " + divisionEntera);
+        System.out.println("Modulo " + modulo);
+        System.out.println("division Real " + divisionReal);
+
+    }
+}
